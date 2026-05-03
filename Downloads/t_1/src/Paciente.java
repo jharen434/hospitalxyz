c80fe6befb46e569664a27daef9d196e6a51efd5
jharen434 <denjharen102938@gmail.com>
2026-05-02 21:57:23 -0500

    inicial

---
+++ Downloads/t_1/src/Paciente.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author daiver
+ */
+public class Paciente {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nombre;
+        String documento;
+        int edad;
+        String sangre;
+        String alergia;
+        String telefono;
+        String email;
+
+                
+    }
+    
+}
